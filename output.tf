06220268cc06697b6158bd1530488c9352d71eec
ankitdattatrayjagtap <ankujagtap20@gmail.com>
2023-08-12 07:57:13 +0530

    Jenkins server working

--- output.tf
+++ output.tf
@@ -1,4 +1,11 @@
 output "VPC-id" {
     value = aws_vpc.our-vpc.id
-  
+}
+
+output "Instance-id" {
+    value = aws_instance.jenkins-server.id
+}
+
+output "Instance-ip" {
+    value = aws_instance.jenkins-server.public_ip
 }
