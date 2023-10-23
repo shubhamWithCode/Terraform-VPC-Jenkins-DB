76301a5798a803fdbf36e5dec798db0d998b6022
ankit-jagtap-devops <ankujagtap20@gmail.com>
2023-10-24 00:28:58 +0530

    kubectl installation added

--- jenkins-server.sh
+++ jenkins-server.sh
@@ -47,3 +47,31 @@ apt update -y
 apt install -y docker.io
 systemctl start docker
 systemctl enable docker
+
+
+apt install unzip -y
+
+# To install AWS CLI V2
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+
+# To install kubectl
+
+ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+ curl -LO https://dl.k8s.io/release/v1.28.3/bin/linux/amd64/kubectl
+
+ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+   
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
+# and then append (or prepend) ~/.local/bin to $PATH
+
